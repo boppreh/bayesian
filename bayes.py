c0cc9c6af3c8e3f63f72ae98518a4c49f65b32bf
BoppreH <boppreh@gmail.com>
2013-05-05 17:32:08 -0300

    Add normal classifier and split samples to file

--- bayes.py
+++ bayes.py
@@ -28,6 +28,58 @@ def classify_file(file_, folders, extractor=str.split):
     new_extractor = lambda f: extractor(open(f).read())
     return classify(file_, classes_instances, new_extractor)
 
+from math import sqrt, pi, exp
+def gaussian_distribution(values):
+    """
+    Given a list of values, returns the (mean, variance) tuple for a
+    Gaussian (normal) distribution.
+    """
+    n = float(len(values))
+    mean = sum(values) / n
+    variance = sum((value - mean) ** 2 for value in values) / (n - 1)
+    return (mean, variance)
+
+def gaussian_probability(sample, distribution):
+    """
+    Given a sample value and the (mean, variance) distribution,
+    return the probability of this sample belonging to the
+    distribution.
+    """
+    mean, variance = distribution
+    return (exp((sample - mean) ** 2 / (-2 * variance))
+            / sqrt(2 * pi * variance))
+
+def properties_distributions(classes_population):
+    """
+    Converts classes populations into classes distributions by property.
+    {class: [{property: value}]} -> {property: {class: distribution}}
+    """
+    properties_distributions = defaultdict(dict)
+    for class_, population in classes_population.items():
+        properties_instances = defaultdict(list)
+        for properties in population:
+            for property, value in properties.items():
+                properties_instances[property].append(value)
+        for property, instances in properties_instances.items():
+            properties_distributions[property][class_] = gaussian_distribution(instances)
+    return properties_distributions
+
+def classify_normal(instance, classes_instances):
+    """
+    Classify `instance` into one of the classes from `classes_instances`,
+    calculating the probabilities from the Gaussian (normal) distribution
+    from each classes' instances.
+
+    classes_instances must be of type {class: [{property: value}]}
+    """
+    distributions = properties_distributions(classes_instances)
+    b = Bayes({class_: .5 for class_ in classes_instances})
+    for property, value in instance.items():
+        classes_distributions = distributions[property]
+        b.update({class_: gaussian_probability(value, distribution)
+                  for class_, distribution in classes_distributions.items()})
+    return b.most_likely()
+        
 
 class Bayes(list):
     """
@@ -196,81 +248,4 @@ class Bayes(list):
         for label, item in zip(self.labels, self.normalized()):
             items.append('{}: {}%'.format(label, round(item * 100, 2)))
         return 'Bayes({})'.format(', '.join(items))
-
-
-if __name__ == '__main__':
-    print ' -- Classic Cancer Test Problem --'
-    # 1% chance of having cancer.
-    b = Bayes([('not cancer', 0.99), ('cancer', 0.01)])
-    # Test positive, 9.6% false positives and 80% true positives
-    b.update((9.6, 80))
-    print b
-    print 'Most likely:', b.most_likely()
-
-    print ''
-    
-    print ' -- Spam Filter With Existing Model --'
-    # Database with number of sightings of each words in (genuine, spam)
-    # emails.
-    words_odds = {'buy': (5, 100), 'viagra': (1, 1000), 'meeting': (15, 2)}
-    # Emails to be analyzed.
-    emails = [
-              "let's schedule a meeting for tomorrow", # 100% genuine (meeting)
-              "buy some viagra", # 100% spam (buy, viagra)
-              "buy coffee for the meeting", # buy x meeting, should be genuine
-             ]
-
-    for email in emails:
-        # Start with priors of 90% chance being genuine, 10% spam.
-        # Probabilities are normalized automatically.
-        b = Bayes([('genuine', 90), ('spam', 10)])
-        # Update probabilities, using the words in the emails as events and the
-        # database of chances to figure out the change.
-        b.update_from_events(email.split(), words_odds)
-        # Print the email and if it's likely spam or not.
-        print email[:15] + '...', b.most_likely()
-
-    print ''
-
-    print ' -- Spam Filter With Email Corpus -- '
-
-    # Email corpus. A hundred spam emails to buy products and with the word
-    # "meeting" thrown around. Genuine emails are about meetings and buying
-    # milk.
-    instances = {'spam': ["buy viagra", "buy cialis"] * 100 + ["meeting love"],
-                 'genuine': ["meeting tomorrow", "buy milk"] * 100}
-
-    # Use str.split to extract features/events/words from the corpus and build
-    # the model.
-    model = Bayes.extract_events_odds(instances, str.split)
-    # Create a new Bayes instance with 10%/90% priors on emails being genuine.
-    b = Bayes({'spam': .9, 'genuine': .1})
-    # Update beliefs with features/events/words from an email.
-    b.update_from_events("buy coffee for meeting".split(), model)
-    # Print the email and if it's likely spam or not.
-    print "'buy coffee for meeting'", ':', b
-
-    print ''
-
-    print ' -- Are You Cheating? -- '
-    results = ['heads', 'heads', 'tails', 'heads', 'heads']
-    events_odds = {'heads': {'honest': .5, 'cheating': .9},
-                   'tails': {'honest': .5, 'cheating': .1}}
-    b = Bayes({'cheating': .5, 'honest': .5})
-    b.update_from_events(results, events_odds)
-    print b
-
-    def b():
-        return Bayes((0.99, 0.01), labels=['not cancer', 'cancer'])
-
-    # Random equivalent examples
-    b() * (9.6, 80)
-    (b() * (9.6, 80)).opposite().opposite()
-    b().update({'not cancer': 9.6, 'cancer': 80})
-    b().update((9.6, 80))
-    b().update_from_events(['pos'], {'pos': (9.6, 80)})
-    b().update_from_tests([True], [(9.6, 80)])
-    Bayes([('not cancer', 0.99), ('cancer', 0.01)]) * (9.6, 80)
-    Bayes({'not cancer': 0.99, 'cancer': 0.01}) * {'not cancer': 9.6,
-                                                   'cancer': 80}
 
