f056733c3c394437e1cd4cd92000f78b75ca3235
BoppreH <boppreh@gmail.com>
2013-10-24 12:56:59 -0200

    Package module

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='Bayesian',
+    version='0.1.0',
+    author='Lucas Boppre Niehues',
+    author_email='lucasboppre@gmail.com',
+    packages=['bayesian'],
+    url='http://pypi.python.org/pypi/Bayesian/',
+    license='LICENSE.txt',
+    description='Utility module for Bayesian reasoning',
+    long_description=open('README.rst').read(),
+)
