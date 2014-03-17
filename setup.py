c5dcc53d98d6ec7e1dc1120c50f423148e3cd03a
BoppreH <boppreh@gmail.com>
2014-03-16 23:56:55 -0300

    Update setup version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='Bayesian',
-    version='0.1.0',
+    version='0.2.0',
     author='Lucas Boppre Niehues',
     author_email='lucasboppre@gmail.com',
     packages=['bayesian'],
