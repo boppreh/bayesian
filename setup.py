dd1d8244a6b2bd8cffebccd0e00a0e74a930abe4
BoppreH <boppreh@gmail.com>
2014-04-05 00:13:15 -0300

    Update SETUP and README

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 
 setup(
     name='Bayesian',
-    version='0.2.0',
+    version='0.2.1',
     author='Lucas Boppre Niehues',
     author_email='lucasboppre@gmail.com',
     packages=['bayesian'],
     url='http://pypi.python.org/pypi/Bayesian/',
     license='LICENSE.txt',
     description='Utility module for Bayesian reasoning',
-    long_description=open('README.md').read(),
+    long_description=open('README.rst').read(),
 )
