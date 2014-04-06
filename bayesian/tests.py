4f6a5965d1e358f9a19e408f16effdeec128c9ad
BoppreH <boppreh@gmail.com>
2014-04-05 22:47:27 -0300

    Add indexing tests

--- bayesian/tests.py
+++ bayesian/tests.py
@@ -20,5 +20,25 @@ class TestBayes(unittest.TestCase):
         with self.assertRaises(ValueError):
             b = Bayes([('a', 10), ('b', 50), ('a', 15)])
 
+    def test_get_odds(self):
+        b = Bayes({'a': 10, 'b': 50})
+        self.assertEqual(b['a'],   10)
+        self.assertEqual(b['b'],   50)
+        self.assertEqual(b[0], 10)
+        self.assertEqual(b[1], 50)
+
+        with self.assertRaises(IndexError):
+            b[2]
+
+        with self.assertRaises(ValueError):
+            b['c']
+
+    def test_set_odds(self):
+        b = Bayes((10, 20, 30))
+        b[0] = 50
+        b[1] = 40
+        b[2] = 30
+        self.assertEqual(b, [50, 40, 30])
+
 if __name__ == '__main__':
     unittest.main()
